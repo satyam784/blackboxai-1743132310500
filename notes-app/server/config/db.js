ae04de800ee55f4fc23ea92f6a70a4418bc64b87
satyam784 <124387721+satyam784@users.noreply.github.com>
2025-03-28 08:57:00 +0530

    Add notes-app/server/config/db.js 1743132417242

---
+++ notes-app/server/config/db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: '../.env' });
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (err) {
+    console.error(`Database connection error: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
